e81a1740f020e1568f7d51e971aafc846cc2cf6e
DestrucMac15 <joungmac4@gmail.com>
2018-12-11 23:07:55 -0600

    crear menu

--- index.php
+++ index.php
@@ -8,7 +8,40 @@
     <?php include('styles.php'); ?>
 </head>
 <body>
+        <div class="container-extended">
+            <div class="row">
+                <div class="col s12">
+                <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Logo</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">Javascript</a></li>
+    <li><a href="mobile.html">Mobile</a></li>
+  </ul>
+                </div>
+            </div>
+        </div>
+
 
     <?php include('scripts.php'); ?>
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        M.AutoInit();
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, options);
+    });
+    </script>
 </body>
 </html>
--- styles.php
+++ styles.php
@@ -1,1 +1,2 @@
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="css/materialize.min.css">
